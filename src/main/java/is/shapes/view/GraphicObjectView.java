6c80541837abeea0728a36a84d977cc37688b3ea
giovanniCRS <gio.caruso26@gmail.com>
2025-02-13 11:19:50 +0100

    Dimenticato di togliere il vecchio applyColor

--- src/main/java/is/shapes/view/GraphicObjectView.java
+++ src/main/java/is/shapes/view/GraphicObjectView.java
@@ -7,7 +7,8 @@ import java.awt.Graphics2D;
 public interface GraphicObjectView {
 	void drawGraphicObject(GraphicObject go, Graphics2D g);
 
+    /* 
 	default void applyColor(Graphics2D g, GraphicObject go) {
         g.setColor(go.getColor());
-    }
+    }*/
 }
